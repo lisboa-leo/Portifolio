6d2dfae1a7be04c83fe77837036fd5e382e456d9
Leonardo Lisboa <leonardo.lisboa.silva.10@gmail.com>
2023-03-13 22:00:49 -0300

    Ajustes e foi adicionado um menu hamburguer para a melhoria do site

---
+++ script.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById("navbar-listas")
+
+        function abrirmenu(){
+            menu.style.right = "0"
+        }
+        function fecharmenu(){
+            menu.style.right = "-200px"
+        }
